14bbb1a804ea28137dc1224c133b192e83874eaf
Sacha Mugnier <smugnier@stbc-i1.nikhef.nl>
2024-03-27 11:02:25 +0100

    added the running script

---
+++ Rivet-JobOptions/running-analysis.sh
@@ -0,0 +1,11 @@
+# This run file is used to run the Rivet analysis on your computer. You can also submit this code to the cluster by
+# typing bsub -J [name of your submission] running-analyis.sh
+
+setupATLAS
+asetup 21.6.77,AthGeneration # or later (please avoid 21.6.19-21.6.32)
+source setupRivet.sh
+
+athena /project/atlas/users/smugnier/RivetRoutine/HWWOffshell_test_ana/JO/HWW_ATLAS_JO_x.py
+
+# here you should put the path to your Rivet job option, I advise you to nicely organise your folders in order to avoid any confusion.
+# The Rivet job option is the file that contains the Rivet analysis you want to run.
